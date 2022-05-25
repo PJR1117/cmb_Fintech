9019ce78321fe3cb2812b837ef6bcf102d585e93
BIG TRUCK <494978022@qq.com>
2022-05-25 10:07:55 +0800

    阶段总结

---
+++ MY_TOOL.py
@@ -0,0 +1,226 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import learning_curve
+from sklearn.model_selection import validation_curve
+from  sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.feature_selection import mutual_info_classif as MIC
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import SelectFromModel
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import roc_curve,auc,roc_auc_score
+from xgboost import XGBClassifier as xgb
+from scipy.stats import boxcox
+from sklearn.impute import KNNImputer
+
+class my_tool():
+    def __init__(self,estimator):
+        self.estimator = estimator
+        self.cv = 5
+
+
+    #EDA
+    def eda(self,data):
+        # 是否平衡
+        print("是否平衡")
+        print(pd.value_counts(data["LABEL"]))
+        print('-' * 50)
+        # 字段类型
+        print("字段类型")
+        print(data.info())
+        print('-' * 50)
+        # 分布情况
+        print("分布情况")
+        print(data.describe())
+        print('-' * 50)
+
+    # 学习曲线
+    def plot_learning_curve(self, title, X, y, ylim=None,
+                            n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):  # n_jobs表示是否进行多进程处理
+        estimator = self.estimator
+        cv = self.cv
+        plt.figure()
+        plt.title(title)
+        if ylim is not None:
+            plt.ylim(*ylim)
+        plt.xlabel("Training examples")
+        plt.ylabel("Score")
+        train_sizes, train_scores, test_scores = learning_curve(
+            estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+        train_scores_mean = np.mean(train_scores, axis=1)
+        train_scores_std = np.std(train_scores, axis=1)
+        test_scores_mean = np.mean(test_scores, axis=1)
+        test_scores_std = np.std(test_scores, axis=1)
+        plt.grid()
+
+        plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                         train_scores_mean + train_scores_std, alpha=0.1,
+                         color="r")
+        plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1, color="g")
+        plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+                 label="Training score")
+        plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+                 label="Validation score")
+
+        plt.legend(loc="best")
+        plt.show()
+        return plt
+
+    # 验证曲线
+    def plot_validation_curve(self, X, y, param_name, param_range, ylim=None, ):
+        estimator = self.estimator
+        cv = self.cv
+        train_scores, test_scores = validation_curve(estimator=estimator, X=X, y=y, param_name=param_name,
+                                                     param_range=param_range, cv=cv)
+        train_mean = np.mean(train_scores, axis=1)
+        train_std = np.std(train_scores, axis=1)
+        test_mean = np.mean(test_scores, axis=1)
+        test_std = np.std(test_scores, axis=1)
+        plt.plot(param_range, train_mean, color='r', marker='o', markersize=5, label='Training score')
+        plt.fill_between(param_range, train_mean + train_std, train_mean - train_std, alpha=0.15, color='r')
+        plt.plot(param_range, test_mean, color='g', linestyle='--', marker='s', markersize=5, label='Validation score')
+        plt.fill_between(param_range, test_mean + test_std, test_mean - test_std, alpha=0.15, color='g')
+        plt.grid()
+        plt.xscale('log')
+        plt.legend(loc='best')
+        plt.xlabel('Parameter')
+        plt.ylabel('Score')
+        plt.ylim(ylim)
+        plt.show()
+
+    # ROC曲线:
+    def plot_roc_curve(self,X_test, y_test):
+        estimator = self.estimator
+        fpr, tpr, threshold = roc_curve(y_test, estimator.predict_proba(X_test)[:, 1])
+        auc = roc_auc_score(y_test, estimator.predict_proba(X_test)[:, 1])
+        plt.plot(fpr, tpr)
+        plt.xlabel('FPR')
+        plt.ylabel('TPR')
+        plt.show()
+        print(round(auc, 5))
+
+#数据清洗
+    #删除缺失值
+    def drop_na(self,data, prop, thresh):
+        if isinstance(data, pd.DataFrame):
+            for i in data.columns:
+                prob_null = sum(pd.isnull(data[i])) / len(data.index)
+                if prob_null >= prop:
+                    del data[i]
+        data = data.dropna(thresh=thresh)
+        return data
+
+    #众数填充
+    def fill_nan_with_mode(self,data):
+        col_index = data.columns
+        mode_list = []
+        for i in col_index:
+            mode = data[i].mode()[0]
+            mode_list.append(mode)
+        dict_results = dict(zip(col_index, mode_list))
+        data.fillna(value=dict_results, inplace=True)
+        return data
+    #KNN填充
+
+
+    #随机森林填充
+
+
+
+    #正态标准化
+    def Normalization(self,data):
+        columns = data.dtypes[data.dtypes == "float64"].index
+        data_sub = data.loc[:,data.dtypes[data.dtypes != "float64"].index]
+        data = data[columns]
+        data = data.aggregate(lambda x:(x-x.mean())/x.std())
+        data = pd.merge(data,data_sub,right_index=True,left_index=True)
+        return data
+
+    #将分类变量转化为哑变量
+    def get_categorial_data(self,data):
+        fill_nan_with_mode = self.fill_nan_with_mode()
+        index = data.dtype[data.dtypes == 'object'].index
+        data_obj = data.loc[:,index].aggregate(lambda x:fill_nan_with_mode(x))
+        for i in data_obj.columns:
+            data_obj.loc[:, i] = pd.Categorical(data_obj[i])
+            del data[i]
+        # data = data.aggregate(lambda x:minmax(x))
+        data_obj = pd.get_dummies(data_obj, drop_first=True)
+        data = pd.merge(data, data_obj, right_index=True, left_index=True)
+        return data
+
+    #minmax变换
+    def minmax(self,data):
+        scal = data.max() - data.min()
+        data_transformed = (data - data.min()) / scal
+        return data_transformed
+
+    # 对非obj进行boxcox变化
+    def get_boxcox(self,data):
+        minmax = self.minmax()
+        columns = data.dtypes[data.dtypes == "float64"].index
+        data_sub = data.loc[:, data.dtypes[data.dtypes != "float64"].index]
+        data = data[columns].aggregate(lambda x:minmax(x))
+        data = data.aggregate(lambda x: boxcox(x + 1)[0])
+        data = pd.merge(data, data_sub, left_index=True, right_index=True)
+        return data
+
+    #生成多项式
+    def get_ploy(self,data,incelude_bias=True,degree=2):
+        ploy = PolynomialFeatures(degree=degree, include_bias=incelude_bias)
+        X_train_ploy = ploy.fit_transform(data)
+        name_list = ploy.get_feature_names()
+        X_train_ploy = pd.DataFrame(X_train_ploy,columns=name_list)
+        return X_train_ploy
+
+    #嵌入法选择(针对树模型)
+    def select_embedded(self,X,y):
+        estimator = self.estimator
+        importance = estimator.fit(X, y).feature_importances_.max()
+        treshold = np.linspace(0, importance, 20)
+        score = []
+        for i in treshold:
+            selector = SelectFromModel(estimator, threshold=i).fit(X, y)
+            X_embedded = selector.transform(X)
+            once = cross_val_score(estimator, X_embedded, y, cv=5).mean()
+            score.append(once)
+        plt.plot(treshold, score)
+        plt.show()
+
+    # #互信息法
+    # def select_mic(X_train, y_train, treshold=0):
+    #     result = MIC(X_train, y_train)
+    #     k = result.shape[0] - sum(result <= treshold)
+    #     selector = SelectKBest(MIC, k).fit(X_train, y_train)
+    #     X_fsmic = selector.transform(X_train, y_train)
+    #     return X_fsmic
+    #
+    # #方差过滤法
+    # def select_var(estimator, X, y, X_test):
+    #     selector = VarianceThreshold(threshold=np.median(X.var())).fit(X)
+    #     X_train_var = selector.transform(X)
+    #     X_test_var = selector.transform(X_test)
+    #     score = cross_val_score(estimator, X, y=y, cv=5).mean()
+    #     return X_train_var, X_test_var, score
+
+    #pca
+    def select_pca(self,X,y):
+        estimator = self.estimator
+        score = []
+        for i in range(X.shape[1]):
+            pca = PCA(n_components=i).fit(X)
+            new_X = pca.transform(X)
+            once = cross_val_score(estimator,new_X,y,cv=5).mean()
+            score.append(once)
+        plt.plot(range(X.shape[1]),score)
+        plt.show()
+
+
--- README.md
+++ README.md
@@ -1,1 +1,235 @@
-# cmb_Fintech
+# 招商银行Fintech
+
+### 目录
+
+
+
+## 一赛题描述
+
+
+
+## 二 方案介绍
+
+```mermaid
+graph LR
+	EDA-->1[特征工程]-->2[建模]-->3[调参]-->4[进阶优化]-->5[总结]
+	subgraph 关键
+	2[建模]--模型表现决定特征工程--->1[特征工程]
+	end
+```
+
+### 1 EDA探索性数据处理
+
+1. 检查label分布,是否平衡[ ]
+
+2. 数据字段类型 df.info() [x]
+
+3. 数据字段分布 df.describe() 主要检查异常值和缺失值[ ],考虑每个字段的意义从而进行判断
+
+   将缺失值可以全部替换成np.nan,dtype==float[ ]
+
+4. 训练集和测试集字段分布的差异,可以用取log来使得差异变小[ ] 
+
+5. 训练集和测试集label分布的差异 对于不平衡的label可以改变权重或者使用过采样[ ]
+
+## 2 数据预处理
+
+
+
+1. 对于标签类字段
+
+```python
+from sklearn.preprocessing import LabelEncoder
+enc = LabelEncoder()
+data=['小猫','小猫','小狗','小狗','兔子','兔子']
+enc=enc.fit(['小猫','小狗','兔子']) #可以使用重复的数,没必要去重
+data=enc.transform(data)
+```
+
+2. 缺失值
+
+3. 如果存在时间类的变量,提取出年月日小时星期是否为周末等字段
+
+
+
+## 3 特征工程
+
+1. **对金额进行挖掘**
+
+* 总金额的log值
+* <u>交易金额的小数部分</u>
+
+2. **对时间挖掘**
+
+* 计算3/7/30/60天的最大金额/最小金额/**<u>平均</u>**/**<u>标准差</u>**/交易次数/总金额/总收入/收入次数/总支出/支出次数/**<u>聚集度特征</u>**
+
+3. **对交易类型进行挖掘**
+
+* 交易方向
+* 支付方式  <u>借记卡信用卡有关的信息</u>
+* 收支一级代码 
+
+4. **从业务角度出发建立特征** *
+
+* RFM模型
+  * 粗粒度刻画：每个id最后一次交易时间、最后一次交易时间是否晚于平均值、交易总次数、多少天有交易行为、交易的总金额、平均每天交易次数、平均每天交易金额、平均每次交易金额；
+  * 进一步细粒度刻画：按照不同交易方向，不同交易方式，以及不同的一级二级交易分类分别聚合分组，计算每个id的最后一次交易时间、最后一次交易时间是否晚于平均值、交易总次数、平均每天交易次数、总金额数、平均每天金额、平均每次金额；
+  * 再进一步细粒度：trd中包含5月和6月两个月用户交易行为，单独考虑信贷用户每月的行为还是很重要的，所以分别对每个月做1) 2)中所述特征统计。
+* word2vec
+
+5. 找毒特???
+
+* 找到测试集和训练集分布不一样的特征然后删除
+
+<!--对特征工程的一些感悟:-->
+
+<!--根据5W1H进行聚合来刻画用户画像,比如说对WHEN进行聚合,每个星期的WHO,WHERE,WHAT,WHY,HOW作为特征/或者对于HOW进行聚合,通过每种方式的WHO,WHAT,WHEN,WHERE,WAY.-->
+
+<!--还可以多种关键词进行聚合,比如WHEN,HOW聚合,每个星期通过这种方法的WHO,WHAT,WHERE,WHY-->
+
+<!--这些都是难以用机器学习完成的工作.-->
+
+* 针对cate类
+  * cate类交叉，num类分桶(等频、等宽、卡方)
+  * 重要的cate类特征做count和转化率特征（注意不要标签泄露）
+  * cate类特征计算评分卡模型中的WOE值，num类卡方分箱后计算WOE
+
+
+
+​	
+
+
+
+## 4 建模
+
+XGBoost
+
+注意要画出每个特征的feature_importances_
+
+
+
+## 5 调参
+
+### 5.1 人工调参
+
+### 5.2 随机搜索
+
+### 5.3 网格搜索
+
+### 5.4 贝叶斯调参(需要学习)
+
+## 6 进阶优化
+
+### 6.1 模型集成
+
+最终使用stacking融合方式，将lightgbm和xgboost的预测结果作为特征，与tag的原始字段一同输入另一个lightgbm模型，使用十折交叉验证得到最终结果。
+
+### 6.2 伪标签(不是很懂)
+
+```python
+n_cv = 5
+kf = StratifiedKFold(n_splits=n_cv, shuffle=True, random_state=666)
+for train_index, test_index in kf.split(x, y):
+    print('*********************')
+    train, valid = train_tag.iloc[train_index], train_tag.iloc[test_index]
+    # psudo中存储的是评测集的数据，其标签是预测的，并不是真正的标签
+    train = train.append(psudo[train.columns], ignore_index=True)
+    train[cat] = train[cat].astype('category')
+    
+    lgb_train = lgb.Dataset(train[x_col], train['flag'])
+    lgb_eval = lgb.Dataset(valid[x_col], valid['flag'], reference=lgb_train)
+    clf = lgb.train(params, lgb_train, 2000, valid_sets = [lgb_train,lgb_eval], verbose_eval=100, early_stopping_rounds=100,
+                    categorical_feature=cat)
+ 
+    clf.predict(valid[x_col],num_iteration=clf.best_iteration)
+    
+    train_model_pred[test_index] = clf.predict(valid[x_col],num_iteration=clf.best_iteration)
+    test_model_pred += clf.predict(test_tag[x_col],num_iteration=clf.best_iteration)
+train_tag['preds'] = train_model_pred
+test_tag['preds'] = test_model_pred/n_cv
+```
+
+## 7 总结
+
+* 中心还是要放在特征工程上面,学习到一波关于违约的"强特" :<u>金额的平均值/标准差/最后一次交易有关的特征/按天统计的特征/对于类别特征还可以统计比例</u>
+
+* 强特之间还可以做交叉,目的使用户之间的差距增大.
+
+* 多学习一些刻画用户画像的模型,如RFM <u>RFM模型是衡量客户价值和客户创利能力的重要工具和手段，客户主要包含三个重要属性：最近一次消费 (Recency)， 消费频率 (Frequency)， 消费金额 (Monetary)。</u>
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
